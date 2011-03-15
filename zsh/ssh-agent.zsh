7f107c6196194d20b9f3d44c45f6dc163707763f
Art Win <artw@users.noreply.github.com>
2011-03-15 08:59:27 +0200

    renamed ssh-agent functions

--- zsh/ssh-agent.zsh
+++ zsh/ssh-agent.zsh
@@ -1,11 +1,11 @@
 SSH_AGENT_ENV=$HOME/.ssh-agent
 
-function ssh-stop {
+function ssh-agent-stop {
    killall ssh-agent
    rm $SSH_AGENT_ENV
 }
 
-function ssh-start {
+function ssh-agent-start {
    if [[ ! -f $SSH_AGENT_ENV ]]; then
      ssh-agent | grep 'export' > $SSH_AGENT_ENV
      chmod 600 $SSH_AGENT_ENV
