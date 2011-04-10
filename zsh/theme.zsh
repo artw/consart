6ace03e9a18048f1726fc4b850a7585c157a58e5
Art Win <art@make.lv>
2011-04-10 22:00:23 +0300

    removed invalid linux ls_colors

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -11,4 +11,3 @@ ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg_bold[red]%}✗"
 
 # LS colors, made with http://geoff.greer.fm/lscolors/
 export LSCOLORS="ExGxcxdxbxegedabagecad"
-export LS_COLORS='di=1;;40:ln=1;;40:so=32;40:pi=33;40:ex=31;40:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=34;42:ow=0;43:'
