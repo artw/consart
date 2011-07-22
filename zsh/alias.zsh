7a0bd9a22843d8e59ed108136e7057599993c1b6
Art Win <artw@users.noreply.github.com>
2011-07-22 10:10:43 +0300

    lots of changes

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -10,7 +10,7 @@ case $OSTYPE in
       alias eject="hdiutil eject"
       alias port="sudo port"
       alias portup="sudo port -v selfupdate"
-      alias portupg="sudo port -v upgrade outdated && sudo port uninstall inactive"
+      alias portupg="sudo port -v upgrade outdated ; sudo port uninstall inactive"
       alias no-guid-is-available="sudo rm /Library/Preferences/SystemConfiguration/NetworkInterfaces.plist"
       ;;
 
