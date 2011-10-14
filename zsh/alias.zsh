62cb1f3f9d77f540d13db84c99fcc3afa0af7995
Art Win <art@make.lv>
2011-10-14 23:59:21 +0300

    added finder alias again

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -16,6 +16,7 @@ case $OSTYPE in
       alias portup="sudo port -v selfupdate"
       alias portupg="sudo port -v upgrade outdated ; sudo port uninstall inactive"
       alias no-guid-is-available="sudo rm /Library/Preferences/SystemConfiguration/NetworkInterfaces.plist"
+      alias finder="open -a finder"
       ;;
 
    freebsd*)
