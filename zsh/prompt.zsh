d3d925d249347608ac2f8aa21403b573903869e5
Art Win <art@make.lv>
2011-10-21 02:01:34 +0300

    clean old shit, cleanup, etc

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -3,9 +3,5 @@ if [ $UID -eq 0 ]; then NCOLOR="red"; else NCOLOR="white"; fi
 #setopt cdablevarS
 setopt prompt_subst
 
-
 PROMPT='%{$fg[$NCOLOR]%}%B%m%b%{$reset_color%}:%{$fg[blue]%}%B%c/%b%{$reset_color%} $(git_prompt_info)%(!.#.$) '
-#RPROMPT='[%*]'
-
-# LS colors, made with http://geoff.greer.fm/lscolors/
-export LSCOLORS="ExGxcxdxbxegedabagecad"
+RPROMPT='[%*]'
