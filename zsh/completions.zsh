9a607792aff258597d922765c24790d6dc225490
Art Win <artw@users.noreply.github.com>
2011-10-25 19:27:40 +0300

    cosmetics

--- zsh/completions.zsh
+++ zsh/completions.zsh
@@ -22,8 +22,8 @@ zstyle ':completion:*:*:kill:*:processes' list-colors "=(#b) #([0-9]#)*=37=31"
 zstyle ':completion:*' completer _expand _complete _ignored _approximate
 
 zstyle ':completion:*' menu select=2
-zstyle ':completion:*' list-colors "=(#b) #([0-9]#)*=37=31"
+zstyle ':completion:*' list-colors "$LS_COLORS"
 zstyle ':completion:*' select-prompt '%SScrolling active: current selection at %p%s'
-zstyle ':completion:*:descriptions' format '%U%F{green}%d%f%u'
+zstyle ':completion:*:descriptions' format '%U%F{blue}%d%f%u'
 
 zstyle ':completion:*:sudo:*' command-path assign /bin /sbin /usr/bin /usr/sbin /usr/local/bin /usr/local/sbin /opt/local/bin /opt/local/sbin
