2dfb272b8712d2f379ad0897576fb4e59a01965d
Art Win <artw@users.noreply.github.com>
2012-01-14 16:15:23 +0200

    check command exists before adding aliases for it; reindent alias.zsh

---
+++ zsh/options.zsh
@@ -0,0 +1,18 @@
+## for use C-S and C-Q in vim maps
+stty -ixon
+## disable stty stop (needed for vim surround plugin)
+stty stop ''
+setopt autocd         # cd if "command" is a directory
+setopt autolist       # automatically list choices on an ambiguous completion
+setopt extendedglob   # globbing extended with # ~ ^
+setopt notify         # report the status of background jobs immediately
+setopt correctall     # try to correct the spelling of all arguments in a line
+setopt nobeep         # disable beep
+setopt autopushd      # pushd on cd
+setopt long_list_jobs # list jobs in the long format by default
+## smart urls
+autoload -U url-quote-magic
+zle -N self-insert url-quote-magic
+
+## jobs
+
