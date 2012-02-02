07db975c2376555826b9d02dd39ac6625f949242
Art Win <artw@users.noreply.github.com>
2012-02-02 10:38:50 +0200

    added coffescript bundle for vim, zsh function for osx logout, changed some aliases, updated youtube-dl

---
+++ zsh/pre.zsh
@@ -0,0 +1,18 @@
+function precmd {
+  title "$HOSTNAME:${PWD/$HOME/~}"
+  echo -ne '\033[?17;0;127c'
+  # on cd add dir to z 
+  _z --add "$(pwd -P)"
+}
+  
+function preexec {
+  emulate -L zsh
+  local -a cmd; cmd=(${(z)1})
+  if [[ $cmd[1]:t == "ssh" ]]; then
+    title $HOSTNAME":" "@"$cmd[2] "$cmd[3,-1]"
+  elif [[ $cmd[1]:t == "sudo" ]]; then
+    title $HOSTNAME":" "#"$cmd[2]:t "$cmd[3,-1]"
+  else
+    title $HOSTNAME":" $cmd[1]:t "$cmd[2,-1]"
+  fi
+}
