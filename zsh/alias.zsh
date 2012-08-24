4720c3c77baa3c171501be176fc9305c4341fefc
Art Win <art@make.lv>
2012-08-24 15:29:29 +0300

    added grub2-cfg alias, changed most alias to global, removing sudo from most. alias cleanup

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -3,6 +3,9 @@ case $OSTYPE in
 linux*)
   alias -g grep="grep --color=auto"
   alias -g ls="ls --color=auto -F"
+  if iscmd grub-mkconfig; then
+    alias -g grub2-cfg="grub-mkconfig -o /boot/grub/grub.cfg"
+  fi
   # arch
   if iscmd yaourt; then
     alias ya="yaourt"
@@ -10,19 +13,19 @@ linux*)
   fi
   # gentoo
   if iscmd emerge; then
-    alias em="sudo emerge"
-    alias emc="sudo emerge"
-    alias esync="sudo eix-sync"
-    alias udav="sudo emerge -uDavN world"
+    alias -g em="emerge"
+    alias -g emc="emerge"
+    alias -g esync="eix-sync"
+    alias -g udav="emerge -uDavN world"
   fi  
   # debian
   if iscmd apt-get; then
-    alias apt="sudo apt-get"
-    alias apti="sudo apt-get install"
-    alias aptr="sudo apt-get remove"
-    alias aptu="sudo apt-get update"
-    alias aptup="sudo apt-get upgrade"
-    alias apts="sudo apt-cache search"
+    alias -g apt="apt-get"
+    alias -g apti="apt-get install"
+    alias -g aptr="apt-get remove"
+    alias -g aptu="apt-get update"
+    alias -g aptup="apt-get upgrade"
+    alias -g apts="apt-cache search"
   fi
 ;;
 
@@ -47,28 +50,26 @@ darwin*)
 
 freebsd*)
   alias -g ls="ls -FIG"
-  alias portupdate="sudo portsnap fetch update"
-  alias pkgi="sudo -E pkg_add -r" 
-  alias pkga="sudo -E pkg_add -r" 
-  alias pkgr="sudo -E pkg_delete" 
-  alias pkgd="sudo -E pkg_delete" 
+  alias -g portupdate="portsnap fetch update"
+  alias -g pkgi="pkg_add -r" 
+  alias -g pkga="pkg_add -r" 
+  alias -g pkgr="pkg_delete" 
+  alias -g pkgd="pkg_delete" 
 ;;
 
 openbsd*)
-  alias pkgi="sudo -E pkg_add -r" 
-  alias pkga="sudo -E pkg_add -r" 
-  alias pkgr="sudo -E pkg_delete" 
-  alias pkgd="sudo -E pkg_delete" 
-  alias cvsupd="sudo cvsup -g -L 2 /etc/cvsupfile"
-  alias out-of-date="sudo /usr/ports/infrastructure/build/out-of-date"
-  alias compile-system="sudo rm -rf /usr/obj/* && cd /usr/src && sudo make obj && cd /usr/src/etc && env DESTDIR=/ sudo make distrib-dirs && cd /usr/src && sudo make build && echo completed!"
-  alias compile-kernel="cd /usr/src/sys/arch/i386/conf/ && sudo config GENERIC && cd ../compile/GENERIC && sudo make clean && sudo make depend && sudo make && sudo make install && echo completed!"
+  alias -g pkgi="pkg_add -r" 
+  alias -g pkga="pkg_add -r" 
+  alias -g pkgr="pkg_delete" 
+  alias -g pkgd="pkg_delete" 
+  alias -g cvsupd="cvsup -g -L 2 /etc/cvsupfile"
+  alias -g out-of-date="/usr/ports/infrastructure/build/out-of-date"
   if which colorls &>/dev/null; then 
     alias ls="colorls -GF"
   fi
-  alias pf="sudo -E pfctl"
-  alias rpf="sudo -E pfctl -f /etc/pf.conf"
-  alias epf="sudo -E $EDITOR /etc/pf.conf"
+  alias pf="pfctl"
+  alias rpf="pfctl -f /etc/pf.conf"
+  alias epf="$EDITOR /etc/pf.conf"
 ;;
 
 solaris*)
@@ -78,19 +79,20 @@ esac
 
 ## universal aliases
 alias -g e=$EDITOR
+alias _e='sudo $EDITOR'
 alias -g ge=gvim
 
 if iscmd sudo; then
   alias sudo="sudo -E"
   alias '#'='sudo'
-  alias '£'='sudo'
+  alias '£''sudo'
 fi
 
 alias rezsh="rehash && source $HOME/.zshrc"
 
-alias mv='nocorrect mv -i'
-alias cp='nocorrect cp -i'
-alias mkdir='nocorrect mkdir'
+alias -g mv='nocorrect mv -i'
+alias -g cp='nocorrect cp -i'
+alias -g mkdir='nocorrect mkdir'
 
 alias -g l='ls -l'
 alias -g la='ls -la'
@@ -98,8 +100,8 @@ alias -g lah='ls -lah'
 
 alias -g dun='du -amx . | sort -n'
 
-alias ...='cd ../..'
-alias ....='cd ../../..'
+alias -g ...='cd ../..'
+alias -g ....='cd ../../..'
 
 if iscmd rsync; then
   alias -g cpr='rsync -avh --progress'
