fb7ea73fb9f3a6364c57608f935dff583383414d
Art Win <artw@users.noreply.github.com>
2013-07-11 00:01:44 +0300

    zsh history tweaks

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -1,17 +1,11 @@
 ## history
 HISTFILE=$HOME/.zsh_history
-HISTSIZE=999
-SAVEHIST=999
-
-setopt share_history
+HISTSIZE=9999
+SAVEHIST=9999
 
 setopt hist_verify
 setopt inc_append_history
-setopt extended_history
+setopt share_history
 setopt hist_ignore_space
 setopt hist_ignore_all_dups
 setopt hist_save_no_dups
-
-setopt SHARE_HISTORY
-setopt APPEND_HISTORY
-
