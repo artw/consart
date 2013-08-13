263dc3c91859ebfde3ecdc53f4bc40b338e718da
Art Win <art@make.lv>
2013-08-13 23:45:34 +0300

    autoload os specific functions only on relevant os functions cleanup

--- zsh/1func.zsh
+++ zsh/1func.zsh
@@ -1,7 +1,22 @@
-## load all custom functions
-fpath=($HOME/.zsh/functions $fpath)
-typeset -U fpath
+## load all functions
 
-for file in $HOME/.zsh/functions/*; do
-   autoload $(basename $file)
-done;
+# generic fpath
+consart_fpath=( $HOME/.zsh/functions $HOME/.zsh/functions/Completion )
+#
+
+# os specific fpath
+consart_fpath_os_specific="$HOME/.zsh/functions/$(uname)" 
+if [[ -d $consart_fpath_os_specific ]]; then
+  consart_fpath+=$consart_fpath_os_specific
+fi
+#
+
+# set fpath
+fpath=($consart_fpath $fpath)
+typeset -U fpath
+#
+
+# autoload all functions within our fpath
+foreach function in $(find $consart_fpath -type f -depth 1); do
+  autoload -Uz $(basename $function)
+done
