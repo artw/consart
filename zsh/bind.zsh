63ba587d154f636807128596f35e81cd4a619435
Art Win <art@make.lv>
2014-03-20 17:15:41 +0200

    minor tweaks and typos

--- zsh/bind.zsh
+++ zsh/bind.zsh
@@ -5,10 +5,9 @@ bindkey -M vicmd "/" history-incremental-pattern-search-backward
 bindkey -M vicmd "?" history-incremental-pattern-search-forward
 bindkey -M vicmd "//" history-beginning-pattern-search-backward
 bindkey -M vicmd "??" history-beginning-pattern-search-forward
-bindkey -M vicmd "q" push-line
-bindkey -M vicmd "u" undo
+bindkey -M vicmd "u" undo               # vi undo is crap
+bindkey -M vicmd "q" push-input         # clear and add this line to history, execute other command before it comes back
 
-bindkey '^K' kill-whole-line
 bindkey "\e[H" beginning-of-line        # Home (xorg)
 bindkey "\e[1~" beginning-of-line       # Home (console)
 bindkey "\e[4~" end-of-line             # End (console)
