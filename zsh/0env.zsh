702c5b58a8f7df3afdb3628c58c8d30889fe1a09
Art Win <art@make.lv>
2015-04-10 12:33:37 +0300

    set PATH before all else

--- zsh/0env.zsh
+++ zsh/0env.zsh
@@ -1,30 +1,3 @@
-# ls colors 
-#useful tool for lscolors: http://geoff.greer.fm/lscolors 
-export LSCOLORS="ExGxcxdxbxegedabagecad"
-export LS_COLORS="di=1;34:ln=1;36:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:"
-
-# enable colors in some tools
-export CLICOLOR=1
-
-# default pager
-which less >/dev/null && export PAGER=less
-
-# set default editor
-if which vim >/dev/null; then
-  export EDITOR=vim
-elif which vi >/dev/null; then
-  export EDITOR=vi
-fi
-
-# hostname (for title function)
-  export HOSTNAME=`hostname -s`
-
-# unbind C-S and C-Q for use in vim maps
-if which stty > /dev/null;then
-  stty -ixon
-  stty stop ''
-fi
-
 # dirs for PATH
 local -a path_dirs
 path_dirs=(
@@ -59,36 +32,6 @@ fpath_dirs=(
   $HOME/.zsh/functions/Completion
   $HOME/.zsh/functions/$(uname)
 )
-
-# fix broken terms (like cygwin)
-export SHELL=$(which zsh)
-
-# add home to cdpath
-cdpath+=$HOME
-
-# os specific environment settings
-case $OSTYPE in
-  darwin*)
-    cdpath+=/Volumes
-  ;;
-  
-  openbsd*)
-    export PKG_PATH=ftp://ftp.eu.openbsd.org/pub/OpenBSD/$(uname -r)/packages/$(uname -m)
-  ;;
-
-  #freebsd*)
-  #;;
-
-  #cygwin*)
-  #;;
-
-  #solaris*)
-  #;;
-  
-  #linux*)
-  #;;
-
-esac
 
 # oracle stuff (if set in zshrc.local) 
 if [[ -d $ORACLE_HOME_DB || -d $ORACLE_HOME_GRID ]]; then
@@ -125,3 +68,60 @@ typeset -U fpath
 foreach function in $(find $fpath_dirs -maxdepth 1 -type f ); do
   autoload -Uz $(basename $function)
 done
+
+# ls colors 
+#useful tool for lscolors: http://geoff.greer.fm/lscolors 
+export LSCOLORS="ExGxcxdxbxegedabagecad"
+export LS_COLORS="di=1;34:ln=1;36:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:"
+
+# enable colors in some tools
+export CLICOLOR=1
+
+# fix broken terms (like cygwin)
+export SHELL=$(which zsh)
+
+# add home to cdpath
+cdpath+=$HOME
+
+# os specific environment settings
+case $OSTYPE in
+  darwin*)
+    cdpath+=/Volumes
+  ;;
+  
+  openbsd*)
+    export PKG_PATH=ftp://ftp.eu.openbsd.org/pub/OpenBSD/$(uname -r)/packages/$(uname -m)
+  ;;
+
+  #freebsd*)
+  #;;
+
+  #cygwin*)
+  #;;
+
+  #solaris*)
+  #;;
+  
+  #linux*)
+  #;;
+
+esac
+
+# default pager
+which less >/dev/null && export PAGER=less
+
+# set default editor
+if which vim >/dev/null; then
+  export EDITOR=vim
+elif which vi >/dev/null; then
+  export EDITOR=vi
+fi
+
+# hostname (for title function)
+  export HOSTNAME=`hostname -s`
+
+# unbind C-S and C-Q for use in vim maps
+if which stty > /dev/null;then
+  stty -ixon
+  stty stop ''
+fi
