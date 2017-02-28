d3d23983518cb50004f5e01f8dbd6bee3f5b8e2d
Art Win <art@make.lv>
2017-02-28 14:58:54 +0200

    more harm than good

--- zsh/option.zsh
+++ zsh/option.zsh
@@ -2,7 +2,7 @@ setopt autocd         # cd if "command" is a directory
 setopt autolist       # automatically list choices on an ambiguous completion
 setopt extendedglob   # globbing extended with # ~ ^
 setopt notify         # report the status of background jobs immediately
-setopt correctall     # correct commands and arguments
+setopt nocorrectall   # do not correct commands and arguments
 setopt nobeep         # disable beep
 setopt autopushd      # pushd on cd
 setopt pushd_minus    # correct stack order with cd -?
