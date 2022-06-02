88811c55561dccb099aa5ac1a98caec2d5dabc77
Art Win <art@make.lv>
2022-06-02 22:26:54 +0200

    sync history

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -4,9 +4,10 @@ HISTSIZE=9999
 SAVEHIST=9999
 
 setopt hist_verify
-setopt append_history
-setopt inc_append_history
-#setopt share_history
+# setopt append_history
+# setopt inc_append_history
+# setopt share_history
 setopt hist_ignore_space
 setopt hist_ignore_all_dups
 setopt hist_save_no_dups
+setopt share_history
