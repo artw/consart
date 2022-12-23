3aaaf12b62ffee71dbf12f503175f5c71a67a8cb
Art Win <art@make.lv>
2022-12-23 10:56:53 +0200

    too slow

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -37,12 +37,12 @@ if [[ -f $zplug ]]; then
   zplug 'zplug/zplug', hook-build:'zplug --self-manage'
 
   # Install plugins if there are plugins that have not been installed
-    if ! zplug check --verbose; then
-        printf "Install? [y/N]: "
-        if read -q; then
-            echo; zplug install
-        fi
-    fi
+  # if ! zplug check --verbose; then
+  #     printf "Install? [y/N]: "
+  #     if read -q; then
+  #         echo; zplug install
+  #     fi
+  # fi
   # load zplug
   zplug load
 
